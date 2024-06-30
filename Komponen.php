f376f04fd06ea864b4f5785486e096e33abb6ec0
Wahyuni Anti <122524103+Wahyunianti@users.noreply.github.com>
2024-06-30 15:38:19 +0700

    Update Komponen.php

--- Komponen.php
+++ Komponen.php
@@ -15,10 +15,10 @@ class Komponen
 
     private function generateUuidV4()
     {
-        $data = random_bytes(4);
-        $hexData = bin2hex($data);
-        $sharedUuid = substr($hexData, 0, 8);
-        return $sharedUuid;
+        $data = random_bytes(16);
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
     }
 
     public function create($judul, $keterangan, $image)
